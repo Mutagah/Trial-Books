c000a0b898d13f3425d035ae444750312371b148
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-12 21:53:27 +0300

    chnaged the booksgebres table naming

--- db/migrate/20220912180500_create_bookgenres.rb
+++ db/migrate/20220912180500_create_bookgenres.rb
@@ -1,6 +1,6 @@
 class CreateBookgenres < ActiveRecord::Migration[7.0]
   def change
-    create_table :book_genres do |t|
+    create_table :bookgenres do |t|
       t.string :book_id
       t.string :genre_id
     end
